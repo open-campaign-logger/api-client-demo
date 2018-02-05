7ff0747cb173bf611bcb23d1d69dfbd021bf865e
Malte Klaumann <mk@jochenlinnemann.de>
2018-02-05 11:04:51 +0100

    Adding comments
    Basic comments on the Javascript side of the demo app, as well as a little bit of cleaning up.

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,15 +1,14 @@
 $(document).ready(function() {
-    //getLogList();
-
+    // This is the click handler that listens for a clickevent
+    // on the button next to the username and password input.
     $( "#initialLoad" ).click(function() {
         getLogList();
     });
-
-
-
-
 });
 
+
+// To delete a log the id is required, the name is only used for Displaypurposes
+// when asking the user to confirm the deletion.
 function deleteLogHandler(id, name) {
     var conf = confirm("Are you sure you want to delete '" + name + "'");
     if (conf === true) {
@@ -25,20 +24,21 @@ function deleteLogHandler(id, name) {
                 getLogList();
             },
             error: function() {
-                alert("Something went wrong, plz investigate");
+                alert("There was an error!");
             }
     
         })
     }
 }
 
+// To change the name of a log we need the log id and its current
+// revision so the backend can keep track of all changes.
 function changeNameHandler(id, rev) {
     var newName = $('#' + id).text();  //returns a jQuery Object
     var data = {
         "rev": rev,
         "name": newName 
     }
-
     $.ajax({
         crossDomain: true,
         url: getBaseUrl() + "/log/" + id,
@@ -54,26 +54,24 @@ function changeNameHandler(id, rev) {
             getLogList();
         },
         error: function() {
-            alert("Something went wrong, plz investigate");
+            alert("There was an error!");
         }
-
     })
-
 }
 
+// To create a new log we don't need any data except for the auth headers.
+// The system sends a response with the data for the newly created log.
+// This data is ignored right now, as we just reload all campaign logs.
 function newLogHandler() {
-    var newLog = $('#newLog').val();  //returns a jQuery Object
+    var newLog = $('#newLog').val();
     var data = {
         "name": newLog 
     }
-
     $.ajax({
         crossDomain: true,
         url: getBaseUrl() + "/log/",
         type: "POST",
         headers: {
-            //"Access-Control-Allow-Headers": "x-requested-with",
-            "Access-Control-Allow-Origin":"*",
             "content-type": "application/json", 
             "CL-Username": getUser(),
             "CL-Password": getPassword()
@@ -83,12 +81,14 @@ function newLogHandler() {
             getLogList();
         },
         error: function() {
-            alert("Something went wrong, plz investigate");
+            alert("There was an error!");
         }
 
     })
 }
 
+// To create a new entry in a log we of course need the logs id 
+// and the rest is created on the fly e.g. Timestamp und uniqueID
 function newLogEntryHandler(id) {
     var newLogEntry = $('#newLogEntry').val();
     var data = {
@@ -113,12 +113,14 @@ function newLogEntryHandler(id) {
             console.log(response)
         },
         error: function(response) {
-            alert("Something went wrong, plz investigate");
+            alert("There was an error!");
         }
 
     })
 }
 
+// To update a log entry requires some data from the edited entry
+// which we supply here through the parameters and then send to the backend.
 function updateLogEntryHandler(logId, entryId, rev, timestamp, uniqueId) {
     var updatedEntry = $('#' + entryId).text();
     var data = {
@@ -143,12 +145,14 @@ function updateLogEntryHandler(logId, entryId, rev, timestamp, uniqueId) {
             getLogEntryList(logId)
         },
         error: function() {
-            alert("Something went wrong, plz investigate");
+            alert("There was an error!");
         }
 
     })
 }
 
+// To delete an entry, we need the parent log id as well as the id of the entry itself
+// The server returns an empty object which gets ignored in this example
 function deleteLogEntryHandler(logId, entryId) {
     var conf = confirm("Are you sure you want to delete this entry? This action is ireversible!");
     if (conf === true) {
@@ -164,66 +168,17 @@ function deleteLogEntryHandler(logId, entryId) {
                 getLogEntryList(logId);
             },
             error: function() {
-                alert("Something went wrong, plz investigate");
+                alert("There was an error!");
             }
     
         })
     }
 }
 
-function showDetailHandler(id, name) {
-    $('#clTabs a[href="#entry"]').tab('show'); // Select tab by name/link
-    $('#detailTitle').html(name);
-    var button = `<button class="btn btn-outline-success" type="button" onclick="newLogEntryHandler('${id}')">Save</button>`;
-    $('#newEntryButton').html(button);
-    getLogEntryList(id);
-}
-
-function getUser() {
-    return $('#user').val();
-}
-function getPassword() {
-    return $('#password').val();
-}
-function getBaseUrl() {
-    return "https://campaign-logger.com/gateway/rest/public"
-}
-
-function getLogEntryList(id) {
-    var user = getUser();
-    var password = getPassword();
-    var url = "https://campaign-logger.com/gateway/rest/public/log/" + id + "/entry";
-    $.ajax({
-        url: url, 
-        type: "GET",
-        headers: {
-            "CL-Username": user,
-            "CL-Password": password
-        },
-        success: function(response){
-            var cardHtml = " ";
-            $.each(response, function (i, item) {
-                cardHtml += `
-                <div class="card">
-                    <div class="card-body">
-                    <h6 class="card-title">Created on: ${item.timestamp}</h5>
-                    <p class="card-text" contenteditable="true" id="${item.id}">${item.htmlText}</p>
-                    </div>
-                    <div class="card-footer text-muted">
-                    <button class="btn btn-outline-success" onclick="updateLogEntryHandler('${id}','${item.id}','${item.rev}','${item.timestamp}', '${item.uniqueId}')">Save</button>
-                    <button class="btn btn-outline-danger" onclick="deleteLogEntryHandler('${id}','${item.id}')">Delete</button>
-                    </div>
-                </div>
-                <br>`
-            });
-            $('#detailCard').html(cardHtml);
-        }
-    });
-}
-
+// This is the most basic request in the API 
+// You just supply username and password and it returns a list 
+// of all log entries.
 function getLogList() {
-    //var user = $('#user').val();
-    //var password = $('#password').val();
     var user = getUser();
     var password = getPassword();
     var url = "https://campaign-logger.com/gateway/rest/public/log";
@@ -256,6 +211,66 @@ function getLogList() {
         }
     });
 }
+
+// this is a basic GET request which just request all entrys associated with a campaign log
+// and then displays them in the second tab of our page.
+function getLogEntryList(id) {
+    var user = getUser();
+    var password = getPassword();
+    var url = "https://campaign-logger.com/gateway/rest/public/log/" + id + "/entry";
+    $.ajax({
+        url: url, 
+        type: "GET",
+        headers: {
+            "CL-Username": user,
+            "CL-Password": password
+        },
+        success: function(response){
+            var cardHtml = " ";
+            $.each(response, function (i, item) {
+                cardHtml += `
+                <div class="card">
+                    <div class="card-body">
+                    <h6 class="card-title">Created on: ${item.timestamp}</h5>
+                    <p class="card-text" contenteditable="true" id="${item.id}">${item.htmlText}</p>
+                    </div>
+                    <div class="card-footer text-muted">
+                    <button class="btn btn-outline-success" onclick="updateLogEntryHandler('${id}','${item.id}','${item.rev}','${item.timestamp}', '${item.uniqueId}')">Save</button>
+                    <button class="btn btn-outline-danger" onclick="deleteLogEntryHandler('${id}','${item.id}')">Delete</button>
+                    </div>
+                </div>
+                <br>`
+            });
+            $('#detailCard').html(cardHtml);
+        }
+    });
+}
+
+
+// ----------------------------------
+// The following are basic helper
+// functions and used for display-
+// purposes and getting data.
+// ----------------------------------
+
+function showDetailHandler(id, name) {
+    $('#clTabs a[href="#entry"]').tab('show'); // Select tab by name/link
+    $('#detailTitle').html(name);
+    var button = `<button class="btn btn-outline-success" type="button" onclick="newLogEntryHandler('${id}')">Save</button>`;
+    $('#newEntryButton').html(button);
+    getLogEntryList(id);
+}
+
+function getUser() {
+    return $('#user').val();
+}
+function getPassword() {
+    return $('#password').val();
+}
+function getBaseUrl() {
+    return "https://campaign-logger.com/gateway/rest/public"
+}
+
 
 // Really simple number generator based on random numbers, for demo purposes only
 // Do not use for your own app!
